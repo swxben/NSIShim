064555ae43b61d28a3b2d8042fa5827ae9b36fcf
Ben Scott <ben@belfryimages.com.au>
2013-02-10 18:41:02 +1000

    fix #4 show md5 hash

--- NSIShim/build-installer.bat
+++ NSIShim/build-installer.bat
@@ -5,6 +5,9 @@ if /i "%1" == "" goto :help
 call "%VS100COMNTOOLS%vsvars32.bat"
 msbuild.exe /ToolsVersion:4.0 "nsishim.sln" /p:configuration=Release
 tools\nsis-2.46\makensis.exe /NOCD /V2 /Dversion=%1 MyCoolAppInstaller.nsi
+echo MD5:
+src\calcmd5\bin\Debug\calcmd5.exe "mycoolapp-%1.exe"
+
 goto :done
 
 
---
+++ NSIShim/src/calcmd5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace calcmd5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!args.Any())
+            {
+                Console.WriteLine("Usage: calcmd5 [filename]");
+                return;
+            }
+
+            var filename = args[0];
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("Cannot find file {0}", filename);
+                return;
+            }
+
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(File.ReadAllBytes(filename));
+            var sb = new StringBuilder();
+            foreach (var b in hash) sb.AppendFormat("{0:X2}", b);
+            Console.WriteLine(sb);
+        }
+    }
+}
